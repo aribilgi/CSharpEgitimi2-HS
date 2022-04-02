1ed0581f4d7b3825bc839f083650e235858257e0
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-04-02 10:54:00 +0300

    2-4-22 hmzrmys

---
+++ MVCEgitim/Controllers/Mvc08ViewToControllerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEgitim.Controllers
+{
+    public class Mvc08ViewToControllerController : Controller
+    {
+        // GET: Mvc08ViewToController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
