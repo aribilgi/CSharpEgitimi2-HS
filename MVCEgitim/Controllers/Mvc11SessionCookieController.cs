841b31cae333240f269941d09a74201908c21cde
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-04-10 14:02:36 +0300

    10-04-2022 Session Cookie Bitti

--- MVCEgitim/Controllers/Mvc11SessionCookieController.cs
+++ MVCEgitim/Controllers/Mvc11SessionCookieController.cs
@@ -18,11 +18,46 @@ namespace MVCEgitim.Controllers
         {
             // Session oluşturup değer atama..
             Session["deger"] = text;
+            Session.Add("seysin", "seysin : " + text); // 2. yöntem
             return View();
         }
         public ActionResult Sessions()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Sessions(string text)
+        {
+            if (Session["deger"] != null) // eğer session boş değilse
+            {
+                Session.Remove("deger"); // deger ismindeki sessionu sil
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CookieOlustur(string kuki)
+        {
+            HttpCookie cookieKullanici = new HttpCookie("kullanici", kuki); //Cookie oluşturma
+            cookieKullanici.Expires = DateTime.Now.AddMinutes(10); // Kukinin yaşam süresi
+            HttpContext.Response.Cookies.Add(cookieKullanici); //Cookie yi istemciye yerleştirme
+            return RedirectToAction("CookieOku"); // sayfayı CookieOku sayfasına yönlendirme
+        }
+        public ActionResult CookieOku()
+        {
+            if (HttpContext.Request.Cookies["kullanici"] == null) // Cookie yi kontrol et
+                ViewBag.Kullanici = "Cookie Yok"; // Eğer kuki yoksa
+            else // Cookie boş değilse
+                ViewBag.Kullanici = "Cookie Değeri : " + HttpContext.Request.Cookies["kullanici"].Value; //Cookie nin değerini okuyup ViewBag e atadık
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CookieSil()
+        {
+            if (HttpContext.Request.Cookies["kullanici"] != null)
+                HttpContext.Response.Cookies["kullanici"].Expires = DateTime.Now.AddSeconds(-3);
+            else ViewBag.Message = "Cookie Yok";
+
+            return RedirectToAction("CookieOku");
         }
     }
 }
