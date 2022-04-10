1da68743387540e97386310ba3067beca01d23a7
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-04-10 10:33:58 +0300

    10-04-2022 Hmz

--- MVCEgitim/Controllers/Mvc10ModelValidationController.cs
+++ MVCEgitim/Controllers/Mvc10ModelValidationController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCEgitim.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,20 @@ namespace MVCEgitim.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult YeniUye()
+        {
+            return View();
+        }
+        [HttpPost] // Aşağıdaki metodun post işleminde çalışacağını belirtir
+        public ActionResult YeniUye(Uye uye) // YeniUye.cshtml sayfasından gönderilen veriler uye class ı ile model binding yöntemiyle bu şekilde parametreyle elde edilebiliyor
+        {
+            if (ModelState.IsValid) // Bu kod ile uye nesnesinde geçerlilik kuralları kontrol edilir
+            {
+                // Eğer uye nesnesinde geçersiz alanlar yoksa buradaki kodlar çalıştırılır
+                ViewBag.UyeBilgi = $"Üye Adı : {uye.Ad} <hr /> Üye Soyad : {uye.Soyad} <hr /> Üye Mail : {uye.Email} <hr /> Üye Tc No : {uye.TCKimlikNo}"; // Eğer bilgiler geçerliyse ViewBag.UyeBilgi ile sayfa ön yüzüne verileri gönderiyoruz
+            }
+            return View(uye); // Eğer parametreyle gelen uye nesnesinde validasyon sorunları varsa return View(uye); koduyla ön yüz sayfasına nesneyi geri gönderiyoruz
         }
     }
 }
