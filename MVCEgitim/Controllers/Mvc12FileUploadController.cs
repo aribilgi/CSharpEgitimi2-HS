385dc25c6fde27dbb5d7361db97bc002f9dea81a
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-04-16 10:33:28 +0300

    16-4-22 dosya

---
+++ MVCEgitim/Controllers/Mvc12FileUploadController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEgitim.Controllers
+{
+    public class Mvc12FileUploadController : Controller
+    {
+        // GET: Mvc12FileUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase dosya)// Mvc de resim yükleme işlemi HttpPostedFileBase sınıfı kullanılarak yapılıyor. Dikkat !! dosya yazan isim ön yüzdeki file upload ın name alanı ile eşleşmeli yoksa resim yüklenmez!
+        {
+            if (dosya != null && dosya.ContentLength > 0)
+            {
+                var extensition = Path.GetExtension(dosya.FileName); // dosya uzantsını al
+                // dosya işlemleri için System.IO kütüphanesini yukarıya using ile eklemeliyiz!
+                if (extensition == ".jpg" || extensition == ".png") // uzantıyı kontrol et
+                {
+                }
+            }
+            return View();
+        }
+    }
+}
